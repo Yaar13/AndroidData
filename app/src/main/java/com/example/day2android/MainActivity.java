d4da5d063e61a61031fa3d1bd204ac4b896ae100
parag <paraggarg66@yahoo.in>
2019-07-26 19:39:44 -0400

    Day 1 Android

--- app/src/main/java/com/example/day2android/MainActivity.java
+++ app/src/main/java/com/example/day2android/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.day2android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,22 +13,41 @@ public class MainActivity extends AppCompatActivity {
     EditText Name;
     EditText Password;
     Button Login;
+    Button Signup;
     TextView info;
-    View info;
-    int counter=5;
+    int counter = 5;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Name = (EditText)findViewById(R.id.EnterEmail);
-        Password = (EditText)findViewById(R.id.EnterPassword);
-        Login = (EditText)findViewById(R.id.Login);
-        info
-
-
-
-
+        Name = (EditText) findViewById(R.id.EnterEmail);
+        Password = (EditText) findViewById(R.id.EnterPassword);
+        Login = (Button) findViewById(R.id.Login);
+        Signup = (Button)findViewById((R.id.Signup));
+        
 
     }
 }
+
+        /*@Override
+        public void onClick(View view) {
+            validate(Name.getText().toString(),Password.getText().toString());
+        }
+    /);
+}
+    void validate(String username,String password){
+        if(username.isEmpty()||username.trim().length()==0){
+            Name.setError("Enter Email id");
+            return;
+        }
+        if(username.equals("abhishek")&&password.equals("1234")){
+            Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+
+            startActivity(intent);
+            finish();
+        }
+    }
+} */
+
